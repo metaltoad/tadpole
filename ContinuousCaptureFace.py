785fe4f75ad24e571f9d893ebb0bb7364120cc33
Michael Beidler <michael.beidler@metaltoad.com>
2017-05-05 06:11:11 +0000

    Confirm face with eye detection

--- ContinuousCaptureFace.py
+++ ContinuousCaptureFace.py
@@ -5,13 +5,15 @@ import cv2
 
 camera = PiCamera()
 camera.resolution = (320, 240)
-camera.framerate = 32
+camera.framerate = 16
 rawCapture = PiRGBArray(camera, size=(320, 240))
 
 time.sleep(0.1)
 
 face_cascade = cv2.CascadeClassifier(
     '/home/pi/opencv-3.0.0/data/haarcascades/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(
+    '/home/pi/opencv-3.0.0/data/haarcascades/haarcascade_eye.xml')
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array
@@ -21,7 +23,13 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 
     for (x,y,w,h) in faces:
-        cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,0),2)
+        # cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,0),2)
+        cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = image[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for(ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
     cv2.imshow("Frame", image)
     key = cv2.waitKey(1) & 0xFF
